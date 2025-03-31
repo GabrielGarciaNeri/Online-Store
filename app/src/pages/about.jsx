329aac8390fb179761c661f3f0df6c109089a388
gabrielgarcianeri <19gjr98@gmail.com>
2025-03-31 15:27:15 -0700

    image fixed in about

--- app/src/pages/about.jsx
+++ app/src/pages/about.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react"
 import "./about.css"
 
-
-
 function About(){
+
     function showData(){
         setVisible(true)
         console.log("button clicked")
     }
+
     const [isVisible, setVisible] = useState(false)
+
     return(
         <div className="about page">
 
-            <img src="../pfp.jpg" alt="about me"></img>
+            <img src="Images/pfp.jpg" alt="about me"></img>
             <h2>Gabriel</h2>
             { isVisible ? <h5>northpole@gmail.com</h5> : null }
             <button className="btn btn-dark" onClick={showData}>See email</button>
